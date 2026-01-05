f1d1ea57858a185dcf253351654a74ed11b32785
sundaram26 <singhsundaram2004@gmail.com>
2026-01-05 22:25:06 +0530

    Home page started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { EventCard } from "@/components/EventCard"
+import { ExploreBtn } from "@/components/ExploreBtn"
+
+const events = [
+  { image: '/images/event1.png', title: 'Event 1' },
+  { image: '/images/event2.png', title: 'Event 2' }
+]
 
 const page = () => {
   return (
@@ -6,8 +13,24 @@ const page = () => {
         The Hub for Every Dev <br/> Event You Can't Miss
       </h1>
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          
+          
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
+
 
 export default page
---
+++ src/components/EventCard.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+    title: string;
+    image: string;
+}
+
+export const EventCard = ({ title, image }: Props) => {
+    return (
+        <Link href={`/events`} id="event-card">
+            <Image src={image} alt={title} width={410} height={300} className="poster" />
+            <p className="title">{title}</p>
+        </Link>
+    )
+}
