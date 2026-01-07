7cfef2ff58a6ef9d4bbd3283e6c1f6fd9a022214
sundaram26 <singhsundaram2004@gmail.com>
2026-01-07 23:37:43 +0530

    Home page completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import { EventCard } from "@/components/EventCard"
 import { ExploreBtn } from "@/components/ExploreBtn"
-
-const events = [
-  { image: '/images/event1.png', title: 'Event 1' },
-  { image: '/images/event2.png', title: 'Event 2' }
-]
+import { EventCard } from "@/components/EventCard"
+import { events } from "@/lib/constants"
 
 const page = () => {
   return (
